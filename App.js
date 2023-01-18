2018fcf0a94d44ba9fa76d524cb8d56201ac61c0
Lucas Moura <93lucasribeiro@gmail.com>
2023-01-18 12:38:32 -0300

    first app

--- App.js
+++ App.js
@@ -1,21 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  TextInput, StyleSheet, ScrollView,
+  Button,
+  View,
+  Text,
+  Linking,
+} from 'react-native';
 
-export default function App() {
+const TextInputExample = () => {
+  const [text, onChangeText] = useState('');
+  const [list, onChangeList] = useState([])
+
+
+  const removeToList = (param) => {
+    const filtered = list.filter((item) => item !== param)
+    onChangeList(filtered)
+  }
+
+  const addTextToList = () => {
+    const newList = [text, ...list]
+    onChangeList(newList)
+    onChangeText('')
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <ScrollView style={styles.body}>
+        <Text style={styles.title}> to do</Text>
+        <Text style={styles.info}>Olá! Este é meu primeiro projeto em React Native. Qualquer sugestão pode ser enviada a partir do link no final da página :)</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text}
+          placeholder="useless placeholder"
+          keyboardType="text"
+          enablesReturnKeyAutomatically={false}
+        />
+        <Button
+          title="add to list"
+          disabled={!text}
+          onPress={() => addTextToList()}
+        />
+        <View style={styles.list}>
+          {list.map((item, index) =>
+            <View style={styles.block} key={index} >
+              <Text style={styles.text}> {item} </Text>
+              <Button
+                title="remove"
+                onPress={() => removeToList(item)}
+              />
+            </View>
+          )
+          }
+        </View>
+
+      </ScrollView>
+        <Text  style={styles.footer} onPress={() => Linking.openURL('https://portfolio-lcsrbr.vercel.app/')}> Criado por Lucas Moura, 2023</Text>
+    </View >
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f5f5dc"
   },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  title: {
+    fontSize: 70,
+    textAlign: 1,
+  },
+  info: {
+    textAlign: 1
+  },
+  text: {
+    fontSize: 20,
+  },
+  block: {
+    display: "flex",
+    flexDirection: "row",
+    textAlign: 1,
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#add8e6",
+    margin: 10
+  },
+  footer: {
+    position: "absolute",
+    display:"flex",
+    alignItems:"center",
+    bottom: 0,
+    right:1,
+    left:1,
+    textAlign: "center",
+  }
 });
+
+export default TextInputExample;
